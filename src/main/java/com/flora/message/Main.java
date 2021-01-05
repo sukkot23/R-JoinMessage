1d0189ea30bc85f869b47c99a751b76f78b911a6
sukkot <mean02463@gmail.com>
2021-01-06 06:41:15 +0900

    Initial commit

--- src/main/java/com/flora/message/Main.java
+++ src/main/java/com/flora/message/Main.java
@@ -1,6 +1,7 @@
 package com.flora.message;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -13,17 +14,21 @@ import java.util.Objects;
 
 public class Main extends JavaPlugin implements Listener
 {
+    String keyPlayer = "`PLAYER`";
+    String keyReason = "`REASON`";
+
     boolean isPrintJoinMessage = true;
     boolean isPrintLeftMessage = true;
     boolean isPrintKickMessage = true;
 
-    String messageFirstJoin = "";
-    String messageJoin = "";
-    String messageLeft = "";
-    String messageKick = "";
+    String messageFirstJoin = "§d`PLAYER` join the game for the first time";
+    String messageJoin = "§e`PLAYER` joined the game";
+    String messageLeft = "§e`PLAYER` left the game";
+    String messageKick = "§f`PLAYER` kicked the game \n&c reason: `REASON`";
 
-    String motdA = "";
+    String motdA = "§7A Minecraft Server";
     String motdB = "";
+
 
 
     @Override
@@ -34,58 +39,60 @@ public class Main extends JavaPlugin implements Listener
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    private void onConfigDataLoad()
-    {
+    private void onConfigDataLoad() {
         isPrintJoinMessage = getConfig().getBoolean("setting.print-JoinMessage");
         isPrintLeftMessage = getConfig().getBoolean("setting.print-LeftMessage");
         isPrintLeftMessage = getConfig().getBoolean("setting.print-KickMessage");
 
-        messageFirstJoin = Objects.requireNonNull(getConfig().getString("messages.first-join")).replaceAll("&", "§");
-        messageJoin = Objects.requireNonNull(getConfig().getString("messages.join")).replaceAll("&", "§");
-        messageLeft = Objects.requireNonNull(getConfig().getString("messages.left")).replaceAll("&", "§");
-        messageKick = Objects.requireNonNull(getConfig().getString("messages.kick")).replaceAll("&", "§");
+        messageFirstJoin = getColorString(Objects.requireNonNull(getConfig().getString("messages.first-join")));
+        messageJoin = getColorString(Objects.requireNonNull(getConfig().getString("messages.join")));
+        messageLeft = getColorString(Objects.requireNonNull(getConfig().getString("messages.left")));
+        messageKick = getColorString(Objects.requireNonNull(getConfig().getString("messages.kick")));
 
-        motdA = Objects.requireNonNull(getConfig().getString("motd.line1")).replaceAll("&", "§");
-        motdB = Objects.requireNonNull(getConfig().getString("motd.line2")).replaceAll("&", "§");
+        motdA = getColorString(Objects.requireNonNull(getConfig().getString("motd.line1")));
+        motdB = getColorString(Objects.requireNonNull(getConfig().getString("motd.line2")));
     }
 
     @EventHandler
-    private void onPlayerJoinEvent(PlayerJoinEvent event)
-    {
+    private void onPlayerJoinEvent(PlayerJoinEvent event) {
         if (isPrintJoinMessage) {
             if (event.getPlayer().hasPlayedBefore())
-                event.setJoinMessage(messageJoin
-                        .replaceAll("%player%", event.getPlayer().getDisplayName()));
+                event.setJoinMessage(getPlayerString(messageJoin, event.getPlayer()));
             else
-                event.setJoinMessage(messageFirstJoin
-                        .replaceAll("%player%", event.getPlayer().getDisplayName()));
+                event.setJoinMessage(getPlayerString(messageFirstJoin, event.getPlayer()));
         }
         else
             event.setJoinMessage("");
     }
 
     @EventHandler
-    private void onPlayerLeftEvent(PlayerQuitEvent event)
-    {
+    private void onPlayerLeftEvent(PlayerQuitEvent event) {
         if (isPrintLeftMessage)
-            event.setQuitMessage(messageLeft
-                    .replaceAll("%player%", event.getPlayer().getDisplayName()));
+            event.setQuitMessage(getPlayerString(messageLeft, event.getPlayer()));
         else
             event.setQuitMessage("");
     }
 
     @EventHandler
-    private void onPlayerKickEvent(PlayerKickEvent event)
-    {
+    private void onPlayerKickEvent(PlayerKickEvent event) {
         if (isPrintKickMessage)
-            event.setLeaveMessage(messageKick
-                    .replaceAll("%player%", event.getPlayer().getDisplayName()
-                    .replaceAll("%reason%", event.getReason())));
+            event.setLeaveMessage(getKickPlayerString(messageKick, event.getPlayer(), event.getReason()));
     }
 
     @EventHandler
-    private void onServerMotdEvent(ServerListPingEvent event)
-    {
+    private void onServerMotdEvent(ServerListPingEvent event) {
         event.setMotd(motdA + "\n" + motdB);
+    }
+
+    private String getColorString(String message) {
+        return message.replaceAll("&", "§");
+    }
+
+    private String getPlayerString(String message, Player player) {
+        return message.replaceAll(keyPlayer, player.getDisplayName());
+    }
+
+    private String getKickPlayerString(String message, Player player, String reason) {
+        return getPlayerString(message, player).replaceAll(keyReason, reason);
     }
 }
